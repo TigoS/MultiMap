03bee640f26c995bcd6fab89c7e4427cfdd41fa8
TigOS <Tigran.A.Karapetyan@gmail.com>
2026-03-11 19:01:47 +0100

    Updating Program

--- MultiMap/Program.cs
+++ MultiMap/Program.cs
@@ -1,13 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using MultiMap.Entities;
+using MultiMap.Helpers;
+using MultiMap.Interfaces;
 
-var multiMap = new MultiMapList<string, int>();
+ISimpleMultiMap<string, int> multiMap1 = TestDataHelper.CreateSampleMultiMap1();
+ISimpleMultiMap<string, int> multiMap2 = TestDataHelper.CreateSampleMultiMap2();
 
-multiMap.Add("A", 1);
-multiMap.Add("A", 2);
-multiMap.Add("B", 3);
+TestDataHelper.PrintMultiMap("MULTI MAP 1", multiMap1);
+TestDataHelper.PrintMultiMap("MULTI MAP 2", multiMap2);
 
-foreach (var value in multiMap.Get("A"))
-{
-    Console.WriteLine(value);
-}
+multiMap1 = TestDataHelper.CreateSampleMultiMap1();
+multiMap1 = multiMap1.MyUnion(multiMap2);
+TestDataHelper.PrintMultiMap("UNION", multiMap1);
+
+multiMap1 = TestDataHelper.CreateSampleMultiMap1();
+multiMap1 = multiMap1.MyIntersect(multiMap2);
+TestDataHelper.PrintMultiMap("INTERSECT", multiMap1);
+
+multiMap1 = TestDataHelper.CreateSampleMultiMap1();
+multiMap1 = multiMap1.MyExceptWith(multiMap2);
+TestDataHelper.PrintMultiMap("EXCEPT WITH 1", multiMap1);
+
+multiMap1 = TestDataHelper.CreateSampleMultiMap1();
+multiMap1 = multiMap2.MyExceptWith(multiMap1);
+TestDataHelper.PrintMultiMap("EXCEPT WITH 2", multiMap1);
+
+multiMap1 = TestDataHelper.CreateSampleMultiMap1();
+multiMap2 = TestDataHelper.CreateSampleMultiMap2();
+multiMap1 = multiMap1.MySymmetricExceptWith(multiMap2);
+TestDataHelper.PrintMultiMap("SYMMETRIC EXCEPT WITH", multiMap1);
+
+Console.ReadLine();
