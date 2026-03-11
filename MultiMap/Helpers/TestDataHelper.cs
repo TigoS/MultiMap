5ce155a1fd3905cecde5e303e97034d120ecf28a
TigOS <Tigran.A.Karapetyan@gmail.com>
2026-03-11 19:01:38 +0100

    Adding test data helper

---
+++ MultiMap/Helpers/TestDataHelper.cs
@@ -0,0 +1,45 @@
+﻿using MultiMap.Entities;
+using MultiMap.Interfaces;
+
+namespace MultiMap.Helpers
+{
+    public class TestDataHelper
+    {
+        public static ISimpleMultiMap<string, int> CreateSampleMultiMap1()
+        {
+            var multiMap = new SimpleMultiMap<string, int>
+            {
+                { "A", 1 },
+                { "A", 2 },
+                { "B", 3 }
+            };
+
+            return multiMap;
+        }
+
+        public static ISimpleMultiMap<string, int> CreateSampleMultiMap2()
+        {
+            var multiMap = new SimpleMultiMap<string, int>
+            {
+                { "A", 1 },
+                { "A", 3 },
+                { "C", 4 },
+                { "C", 3 },
+            };
+
+            return multiMap;
+        }
+
+        public static void PrintMultiMap(string title, ISimpleMultiMap<string, int> multiMap)
+        {
+            Console.WriteLine($"{title}:");
+
+            foreach (var item in multiMap.Flatten())
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
