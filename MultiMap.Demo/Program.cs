5e5ec0017698b932e5efa16cdb3cc4a9088f76c2
TigOS <Tigran.A.Karapetyan@gmail.com>
2026-04-06 16:32:26 +0200

    Updating NuGet 1.0.8->1.0.9

--- MultiMap.Demo/Program.cs
+++ MultiMap.Demo/Program.cs
@@ -1,32 +1,33 @@
 // See https://aka.ms/new-console-template for more information
+using MultiMap.Demo;
 using MultiMap.Helpers;
 using MultiMap.Interfaces;
 
-ISimpleMultiMap<string, int> multiMap1 = MultiMap.Demo.TestDataHelper.CreateSampleMultiMap1();
-ISimpleMultiMap<string, int> multiMap2 = MultiMap.Demo.TestDataHelper.CreateSampleMultiMap2();
+ISimpleMultiMap<string, int> multiMap1 = TestDataHelper.CreateSampleMultiMap1();
+ISimpleMultiMap<string, int> multiMap2 = TestDataHelper.CreateSampleMultiMap2();
 
-MultiMap.Demo.TestDataHelper.PrintMultiMap("MULTI MAP 1", multiMap1);
-MultiMap.Demo.TestDataHelper.PrintMultiMap("MULTI MAP 2", multiMap2);
+TestDataHelper.PrintMultiMap("MULTI MAP 1", multiMap1);
+TestDataHelper.PrintMultiMap("MULTI MAP 2", multiMap2);
 
-multiMap1 = MultiMap.Demo.TestDataHelper.CreateSampleMultiMap1();
+multiMap1 = TestDataHelper.CreateSampleMultiMap1();
 multiMap1 = multiMap1.Union(multiMap2);
-MultiMap.Demo.TestDataHelper.PrintMultiMap("UNION", multiMap1);
+TestDataHelper.PrintMultiMap("UNION", multiMap1);
 
-multiMap1 = MultiMap.Demo.TestDataHelper.CreateSampleMultiMap1();
+multiMap1 = TestDataHelper.CreateSampleMultiMap1();
 multiMap1 = multiMap1.Intersect(multiMap2);
-MultiMap.Demo.TestDataHelper.PrintMultiMap("INTERSECT", multiMap1);
+TestDataHelper.PrintMultiMap("INTERSECT", multiMap1);
 
-multiMap1 = MultiMap.Demo.TestDataHelper.CreateSampleMultiMap1();
+multiMap1 = TestDataHelper.CreateSampleMultiMap1();
 multiMap1 = multiMap1.ExceptWith(multiMap2);
-MultiMap.Demo.TestDataHelper.PrintMultiMap("EXCEPT WITH 1", multiMap1);
+TestDataHelper.PrintMultiMap("EXCEPT WITH 1", multiMap1);
 
-multiMap1 = MultiMap.Demo.TestDataHelper.CreateSampleMultiMap1();
+multiMap1 = TestDataHelper.CreateSampleMultiMap1();
 multiMap1 = multiMap2.ExceptWith(multiMap1);
 MultiMap.Demo.TestDataHelper.PrintMultiMap("EXCEPT WITH 2", multiMap1);
 
-multiMap1 = MultiMap.Demo.TestDataHelper.CreateSampleMultiMap1();
-multiMap2 = MultiMap.Demo.TestDataHelper.CreateSampleMultiMap2();
+multiMap1 = TestDataHelper.CreateSampleMultiMap1();
+multiMap2 = TestDataHelper.CreateSampleMultiMap2();
 multiMap1 = multiMap1.SymmetricExceptWith(multiMap2);
-MultiMap.Demo.TestDataHelper.PrintMultiMap("SYMMETRIC EXCEPT WITH", multiMap1);
+TestDataHelper.PrintMultiMap("SYMMETRIC EXCEPT WITH", multiMap1);
 
 Console.ReadLine();
