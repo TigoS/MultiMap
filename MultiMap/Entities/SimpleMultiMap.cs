6aacbf004e75cbfe4ec02b23b94b19afed5bf538
TigOS <Tigran.A.Karapetyan@gmail.com>
2026-08-27 15:20:15 +0200

    Mark SimpleMultiMap [Obsolete]

--- MultiMap/Entities/SimpleMultiMap.cs
+++ MultiMap/Entities/SimpleMultiMap.cs
@@ -16,6 +16,7 @@ namespace MultiMap.Entities
     /// </remarks>
     /// <typeparam name="TKey">The type of keys in the map. Must be non-nullable and implement <see cref="IEquatable{TKey}"/>.</typeparam>
     /// <typeparam name="TValue">The type of values associated with each key. Must be non-nullable and implement <see cref="IEquatable{TValue}"/>.</typeparam>
+    [Obsolete("SimpleMultiMap<TKey, TValue> is deprecated. Use MultiMapSet<TKey, TValue> instead, which provides the full IMultiMap contract with a richer API.")]
     public sealed class SimpleMultiMap<TKey, TValue> : ISimpleMultiMap<TKey, TValue>
         where TKey : notnull, IEquatable<TKey>
         where TValue : notnull, IEquatable<TValue>
